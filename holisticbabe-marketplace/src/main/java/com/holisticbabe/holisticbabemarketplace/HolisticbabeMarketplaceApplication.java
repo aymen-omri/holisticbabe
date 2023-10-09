5393c7d9df973fc549e3f46766b4d44118aeb091
Aymen Omri <aymenomri14.03@gmail.com>
2023-10-09 11:43:12 +0100

    starting...

---
+++ holisticbabe-marketplace/src/main/java/com/holisticbabe/holisticbabemarketplace/HolisticbabeMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package com.holisticbabe.holisticbabemarketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolisticbabeMarketplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolisticbabeMarketplaceApplication.class, args);
+	}
+
+}
---
+++ holisticbabe-marketplace/src/test/java/com/holisticbabe/holisticbabemarketplace/HolisticbabeMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.holisticbabe.holisticbabemarketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolisticbabeMarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
